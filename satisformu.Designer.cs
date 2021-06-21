6568cfb68f3ab8db9d0ad747a9c6c20f58763440
bburak46 <brkylmz046@gmail.com>
2021-06-21 16:43:50 +0300

    CASE-B113 Satış formu frontend
    satış formu tasarım ekranı yapıldı eklendi

---
+++ satisformu.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ilk_giris_ekranı
+{
+    partial class satisformu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // satisformu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 706);
+            this.Name = "satisformu";
+            this.Text = "satisformu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
