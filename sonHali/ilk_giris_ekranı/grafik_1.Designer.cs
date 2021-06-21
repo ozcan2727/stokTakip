5ed44e5eb3ea8c6ebcd648c21b07167d34db892f
ozcan <snmz9000@gmail.com>
2021-06-21 14:29:16 +0300

    case-B111-Grafik1-Desinger
    satış listesi için grafik gösterimi ekleme çıkarma filtreleme gibi özeliklerle beraber

---
+++ sonHali/ilk_giris_ekranı/grafik_1.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace ilk_giris_ekranı
+{
+    partial class grafik_1
+    {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1_;
+        private System.Windows.Forms.DataGridView dataGridView1_per_detay;
+        private carilerDataSet40 carilerDataSet20;
+        private System.Windows.Forms.BindingSource carisatiskaydiBindingSource;
+        private carilerDataSet40TableAdapters.cari_satis_kaydiTableAdapter cari_satis_kaydiTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn toplamtutarDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personeladiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2_per_detay;
+        private carilerDataSet41 carilerDataSet19;
+        private System.Windows.Forms.BindingSource persatiskaydiBindingSource;
+        private carilerDataSet41TableAdapters.per_satis_kaydiTableAdapter per_satis_kaydiTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn toplamtutarDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personeladiDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
