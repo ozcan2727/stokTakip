7574dcb253a010f475fd29fcd0fa4bbee96a9665
ozcan <snmz9000@gmail.com>
2021-06-22 22:41:59 +0300

    CASE-B117-hatırlatıcı
    harılatıcı classının desinger hali labeller ve görünümü

---
+++ sonHali/ilk_giris_ekranı/hatırlatıcı.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ilk_giris_ekranı
+{
+    partial class HATIRLATICI
+    {
+     
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HATIRLATICI));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button_alarm = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowWeekNumbers = true;
+            this.monthCalendar1.TabIndex = 13;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(18, 237);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(350, 72);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // button_alarm
+            // 
+            this.button_alarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_alarm.Image = ((System.Drawing.Image)(resources.GetObject("button_alarm.Image")));
+            this.button_alarm.Location = new System.Drawing.Point(18, 342);
+            this.button_alarm.Name = "button_alarm";
+            this.button_alarm.Size = new System.Drawing.Size(350, 69);
+            this.button_alarm.TabIndex = 16;
+            this.button_alarm.Text = "ALARM KUR";
+            this.button_alarm.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button_alarm.UseVisualStyleBackColor = true;
+            this.button_alarm.Click += new System.EventHandler(this.button_alarm_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(18, 424);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(350, 38);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "KAPAT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(18, 315);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(115, 21);
+            this.radioButton1.TabIndex = 19;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "SES ÇALMA";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // HATIRLATICI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 474);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button_alarm);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "HATIRLATICI";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HATIRLATICI";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.hatırlatıcı_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button_alarm;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
